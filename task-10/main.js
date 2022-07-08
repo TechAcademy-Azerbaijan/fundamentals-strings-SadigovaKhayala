2a22feac09536558dadb0a58cb15cc6d0513cfb9
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-07-08 20:52:07 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -1,9 +1,98 @@
-const prompt = require('prompt');
-prompt.start();
+// TASK 4
+// let str="Hello world! I learn to code."
+// let count=0;
+// for(i=0;i<str.length;i++){
+//     if(str[i]=='!'){
+//         count++
+//     }
+//     else if(str[i]=='.')
+//        count++
+//     }
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+
+// console.log(count)
+
+//TASK 5
+
+// let str="+5-2+4-m/n*2:4"
+// let count=0;
+// let temp=[];
+// let i;
+
+// for( j=0;j<str.length;j++){
+//     if(!temp.includes(str[j])){
+//         temp.push(str[j])
+//     }
+// }
+
+
+// for( i=0;i<temp.length;i++){
+//     if(temp[i]=='+'||temp[i]=='-'||temp[i]=='/'||temp[i]=='*'||temp[i]==':'){
+//         count++
+//     }}
+
+//     console.log(count)
+// TASK 6 
+
+// let str='Hello world! Hello,    country!';
+// let word;
+// let count=0;
+// for(i=0;i<str.length;i++){
+// if(str[i]==" "&& str[i+1]!=" "){
+//     count++
+
+// }}
+
+// console.log(count+1)
+
+
+// TASK 7
+
+// let str='kpkkpkk';
+// let word=str.split('p');
+// let a= -Infinity
+
+// for( i=0;i<word.length;i++){
+//         if(word[i].length>a){
+//             a=word[i].length
+
+//     }
+// }
+
+// console.log(a);
+
+
+//TASK 8 
+
+// let str='introduction to algorithms';
+// let words=str.split(' ')
+// let temp;
+
+// for(i=0;i<words.length;i++){
+//     words[i]=words[i][0].toUpperCase()+words[i].slice(1)
+
+
+// }
+// console.log(words.join(' '))
+
+//task 9
+// let s = "This is a cat sitting on a table"
+// let c = "t"
+// let d=c.toUpperCase()
+// let count=0;
+
+// for(i=0;i<s.length;i++){
+//     if(s[i]==c||s[i]==d){
+//         count++
+//     }
+// }
+// console.log(count)
+
+//task 10 
+
+// const str='I am programming on Javascript.'
+// const first=str.indexOf(' ')
+// const last=str.lastIndexOf(' ')
+
+// console.log(first,last)
